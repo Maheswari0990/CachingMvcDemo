c4bb4bcdbf1465e32ce7e2fb15b6ee1d3e41745f
THUDIMILLA MAHESWARI <tlakshmil567@gmail.com>
2021-06-03 15:22:26 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CachingMvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(Duration = 10,VaryByParam = "None")]
+        public ActionResult ShowData()
+        {
+            ViewBag.TimeofCache = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult ShowData(int id)
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
